f59147de8e34687e1d818a28f078439469acfcc5
LucaPinheiro <lucapgomes11@gmail.com>
2024-05-26 22:57:57 -0300

    refactor: Update Transaction class constructor and validation methods

--- src/app/entities/transaction.py
+++ src/app/entities/transaction.py
@@ -2,22 +2,34 @@ from typing import Tuple
 from src.app.errors.entity_errors import ParamNotValidated
 from src.app.enums.transaction_type_enum import TransactionTypeEnum
 
+
 class Transaction:
-    account: str
+    account_destiny: str
     value: float
     transaction_type: TransactionTypeEnum
 
-    def __init__ (self, value: float, transaction_type: TransactionTypeEnum):
+    def __init__(self, account_destiny: str, value: float, transaction_type: TransactionTypeEnum):
+        self.account_destiny = account_destiny
         validation_price = self.validate_price(value)
         if validation_price[0] is False:
             raise ParamNotValidated("price", validation_price[1])
         self.value = value
 
-        validation_transaction_type = self.validate_transaction_type(transaction_type)
+        validation_transaction_type = self.validate_transaction_type(
+            transaction_type)
         if validation_transaction_type[0] is False:
-            raise ParamNotValidated("transaction_type", validation_transaction_type[1])
+            raise ParamNotValidated(
+                "transaction_type", validation_transaction_type[1])
         self.transaction_type = transaction_type
-    
+
+    @staticmethod
+    def validate_account_destiny(account_destiny: str) -> Tuple[bool, str]:
+        if account_destiny is None:
+            return (False, "account is required")
+        if type(account_destiny) != str:
+            return (False, "account must be a string")
+        return (True, "")
+
     @staticmethod
     def validate_price(price: float) -> Tuple[bool, str]:
         if price is None:
@@ -27,7 +39,7 @@ class Transaction:
         if price < 0:
             return (False, "price must be greater than 0")
         return (True, "")
-    
+
     @staticmethod
     def validate_transaction_type(transaction_type: TransactionTypeEnum) -> Tuple[bool, str]:
         if transaction_type is None:
@@ -35,7 +47,7 @@ class Transaction:
         if type(transaction_type) != TransactionTypeEnum:
             return (False, "transaction_type must exist in TransactionTypeEnum")
         return (True, "")
-    
+
     def to_dict(self):
         return {
             "price": self.price,
