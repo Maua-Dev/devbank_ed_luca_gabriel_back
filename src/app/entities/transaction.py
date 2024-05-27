29f51a1a20bffa3a44e1163aae47cda79e160b19
Gabriel Merola <gabrielmerola12@gmail.com>
2024-05-26 23:33:09 -0300

    remove validation

--- src/app/entities/transaction.py
+++ src/app/entities/transaction.py
@@ -1,26 +1,19 @@
 from typing import Tuple
 from src.app.errors.entity_errors import ParamNotValidated
-from src.app.enums.transaction_type_enum import TransactionTypeEnum
 
 
 class Transaction:
     account_destiny: str
     value: float
-    transaction_type: TransactionTypeEnum
+    transaction_type: str
 
-    def __init__(self, account_destiny: str, value: float, transaction_type: TransactionTypeEnum):
+    def __init__(self, account_destiny: str, value: float, transaction_type: str):
         self.account_destiny = account_destiny
         validation_price = self.validate_price(value)
         if validation_price[0] is False:
             raise ParamNotValidated("price", validation_price[1])
         self.value = value
-
-        # validation_transaction_type = self.validate_transaction_type(
-        #     transaction_type)
-        # if validation_transaction_type[0] is False:
-        #     raise ParamNotValidated(
-        #         "transaction_type", validation_transaction_type[1])
-        # self.transaction_type = transaction_type
+        self.transaction_type = transaction_type
 
     @staticmethod
     def validate_account_destiny(account_destiny: str) -> Tuple[bool, str]:
@@ -39,14 +32,6 @@ class Transaction:
         if price < 0:
             return (False, "price must be greater than 0")
         return (True, "")
-
-    # @staticmethod
-    # def validate_transaction_type(transaction_type: TransactionTypeEnum) -> Tuple[bool, str]:
-    #     if transaction_type is None:
-    #         return (False, "transaction_type is required")
-    #     if type(transaction_type) != TransactionTypeEnum:
-    #         return (False, "transaction_type must exist in TransactionTypeEnum")
-    #     return (True, "")
 
     def to_dict(self):
         return {
