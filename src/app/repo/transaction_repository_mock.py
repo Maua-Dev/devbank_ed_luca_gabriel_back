221c3b6a9d54eec57a85a2986c7f16b8fb9ee7bf
LucaPinheiro <lucapgomes11@gmail.com>
2024-05-26 22:49:43 -0300

    feat: Add TransactionRepositoryMock class

---
+++ src/app/repo/transaction_repository_mock.py
@@ -0,0 +1,20 @@
+from typing import Dict
+
+from src.app.entities.transaction import Transaction
+from src.app.repo.transaction_repository_interface import ITransactionRepository
+
+
+class TransactionRepositoryMock(ITransactionRepository):
+    transactions: Dict[int, Transaction]
+
+    def __init__(self):
+        self.transactions = {
+            1: Transaction(account="12345-5", value=120.0, transaction_type="deposit"),
+            2: Transaction(account="11111-2", value=10.0, transaction_type="withdraw"),
+            3: Transaction(account="81638-1", value=0.0, transaction_type="deposit"),
+            4: Transaction(account="91739-9", value=2310.0, transaction_type="withdraw"),
+            5: Transaction(account="91783-6", value=12393.0, transaction_type="deposit")
+        }
+
+    def post_transaction(self, account: str, value: float, transaction_type: str) -> Transaction:
+        pass
