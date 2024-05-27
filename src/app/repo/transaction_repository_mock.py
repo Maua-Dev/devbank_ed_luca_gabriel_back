ebfa35b224bcd66f915629b629168d07eb7f954a
LucaPinheiro <lucapgomes11@gmail.com>
2024-05-26 22:58:09 -0300

    refactor: Update account parameter name in post_transaction method

--- src/app/repo/transaction_repository_mock.py
+++ src/app/repo/transaction_repository_mock.py
@@ -9,12 +9,12 @@ class TransactionRepositoryMock(ITransactionRepository):
 
     def __init__(self):
         self.transactions = {
-            1: Transaction(account="12345-5", value=120.0, transaction_type="deposit"),
-            2: Transaction(account="11111-2", value=10.0, transaction_type="withdraw"),
-            3: Transaction(account="81638-1", value=0.0, transaction_type="deposit"),
-            4: Transaction(account="91739-9", value=2310.0, transaction_type="withdraw"),
-            5: Transaction(account="91783-6", value=12393.0, transaction_type="deposit")
+            1: Transaction(account_destiny="12345-5", value=120.0, transaction_type="deposit"),
+            2: Transaction(account_destiny="11111-2", value=10.0, transaction_type="withdraw"),
+            3: Transaction(account_destiny="81638-1", value=0.0, transaction_type="deposit"),
+            4: Transaction(account_destiny="91739-9", value=2310.0, transaction_type="withdraw"),
+            5: Transaction(account_destiny="91783-6", value=12393.0, transaction_type="deposit")
         }
 
-    def post_transaction(self, account: str, value: float, transaction_type: str) -> Transaction:
+    def post_transaction(self, account_destiny: str, value: float, transaction_type: str) -> Transaction:
         pass
