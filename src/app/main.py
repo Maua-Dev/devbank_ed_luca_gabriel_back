29d2061b123fc4527982520f28d56d776b28e975
LucaPinheiro <lucapgomes11@gmail.com>
2024-05-26 22:34:17 -0300

    chore: Refactor main.py for improved readability and maintainability

--- src/app/main.py
+++ src/app/main.py
@@ -1,23 +1,41 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from mangum import Mangum
-
+from repo.client_repository_interface import IClientRepository
+from src.app.repo.transaction_repository_interface import ITransactionRepository
 from .environments import Environments
-
 from .errors.entity_errors import ParamNotValidated
 
 app = FastAPI()
 
+
+class MyApp:
+    def __init__(self, client_repository: IClientRepository, transaction_repository: ITransactionRepository):
+        self.client_repository = client_repository
+        self.transaction_repository = transaction_repository
+
+
 repo = Environments.get_item_repo()()
+my_app = MyApp(repo)
+
 
 @app.get("/")
-def get_client():
-    return {
-        "API": "OK"
-    }
+def get_client(self, account: str):
+    try:
+        client = self.client_repository.get_client(account)
+        if client is None:
+            raise HTTPException(status_code=404, detail="Client not found")
+        return client
+    except ParamNotValidated as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
 
 @app.post("/deposit")
-def post_transaction():
-    return {}
+def post_transaction(self, account: str, value: float, transaction_type: str = Query(None)):
+    try:
+        self.transaction_repository.post_transaction(
+            account, value, transaction_type)
+    except ParamNotValidated as e:
+        raise HTTPException(status_code=400, detail=str(e))
 
 
 handler = Mangum(app, lifespan="off")
